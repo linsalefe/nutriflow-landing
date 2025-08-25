93452a6e129da301f81353e7a0f86395efeae3f5
Álefe Lins <alefelins01@gmail.com>
2025-08-25 15:44:54 -0300

    feat: minha alteração

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+// src/app/page.tsx
 'use client';
 
 import React from 'react';
@@ -5,15 +6,12 @@ import { Container, Box } from '@mui/material';
 import Header from '../components/landing/Header';
 import Hero from '../components/landing/Hero';
 import FloatingCTA from '../components/landing/FloatingCTA';
-import Countdown from '../components/landing/Countdown';
 import TrustBadges from '../components/landing/TrustBadges';
-import Demo from '../components/landing/Demo';
 import Features from '../components/landing/Features';
 import Testimonials from '../components/landing/Testimonials';
 import PricingTeaser from '../components/landing/PricingTeaser';
 import FAQ from '../components/landing/FAQ';
 import Footer from '../components/landing/Footer';
-import ChatbotAI from '../components/landing/ChatbotAI';
 import BackToTop from '../components/landing/BackToTop';
 
 export default function HomePage() {
@@ -24,9 +22,7 @@ export default function HomePage() {
       <FloatingCTA />
       <Container maxWidth="lg" disableGutters>
         <Box component="main">
-          <Countdown />
           <TrustBadges />
-          <Demo />
           <Box id="features"><Features /></Box>
           <Box id="testimonials"><Testimonials /></Box>
           <Box id="pricing"><PricingTeaser /></Box>
@@ -34,7 +30,6 @@ export default function HomePage() {
         </Box>
         <Footer />
       </Container>
-      <ChatbotAI />
       <BackToTop />
     </>
   );
